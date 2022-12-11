14b7ecaf74ca46929e39b8c9bbdc465062d79640
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2022-12-11 22:33:45 +0530

    feat: added csv parser

--- controllers/micpController.js
+++ controllers/micpController.js
@@ -1,5 +1,5 @@
 const Micp = require("../models/micp");
-const {populate, updateRatingsAndScores} = require("../scrap");
+const {updateRatingsAndScores, fetchData} = require("../scrap");
 
 exports.getRankList = async (req, res) => {
     const users = await Micp.find({}, '_id username score currentRating name').sort({ score: -1, currentRating: -1 });
@@ -12,7 +12,7 @@ exports.getRankList = async (req, res) => {
 };
 
 exports.refreshRankList = async (req, res) => {
-    await populate();
+    await fetchData();
     await updateRatingsAndScores();
 
     // console.log(users);
