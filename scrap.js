1de3cf9fce4745b38ce33e32a86135ad4fd673e7
Akshat Nema <76521428+akshatnema@users.noreply.github.com>
2022-12-23 00:08:54 +0530

    fix: Promises implemented (#7)

--- scrap.js
+++ scrap.js
@@ -1,24 +1,29 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const Micp = require("./models/micp");
-const {parse} = require('csv-parse');
+const { parse } = require('csv-parse');
 const getRating = async (username) => {
   const url = `https://www.codechef.com/users/${username}`;
-  const response = await axios.get(url);
-  const $ = cheerio.load(response.data);
-  const rating = $(".rating-number").text();
-  if (rating === "") return false;
-  return parseInt(rating);
+  try{
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const rating = $(".rating-number")?.text() ? $(".rating-number")?.text() : "";
+    if (rating === "") return false;
+    return parseInt(rating);
+  }catch(err){
+    console.log(err);
+    return false;
+  }
 };
 
 // To populate the database with initial rating and score of new users
 
 const populate = async (dat) => {
   let count = 0;
-  for(const user of dat) {
+  for (const user of dat) {
     console.log(++count);
     const id = user["ID"];
-    const micp = await Micp.findOne({ username:id });
+    const micp = await Micp.findOne({ username: id });
     if (!micp) {
       const rating = await getRating(id);
       if (rating !== false) {
@@ -38,45 +43,62 @@ const populate = async (dat) => {
 // cron job to update the score of users every 24 hours
 const updateRatingsAndScores = async () => {
   const users = await Micp.find();
-  const promises = users.map(async (user, i) => {
-    const rating = await getRating(user.username);
-    console.log(i)
-    if (rating > user.currentRating) {
-      user.score += rating - user.currentRating;
-      user.initialRating = user.currentRating;
-      user.currentRating = rating;
-
-      await user.save();
-    } else if (rating < user.currentRating) {
-      user.score -= user.currentRating - rating;
-      user.initialRating = user.currentRating;
-      user.currentRating = rating;
-      if (user.score < 0) user.score = 0;
-      await user.save();
-    }
+  let promises = [];
+  try{
+  users.forEach(async (user, i) => {
+    const url = `https://www.codechef.com/users/${user.username}`;
+    let promise = new Promise(async (resolve, reject) => {
+      let response = await axios.get(url)
+      resolve({
+        user: user,
+        response: response
+      })
+    })
+    promises.push(promise)
   });
-  await Promise.all(promises);
+  await Promise.all(promises).then((data) => {
+    data.forEach(async (item) => {
+      let user = item.user;
+      let $ = cheerio.load(item.response.data);
+      let rating = parseInt($(".rating-number")?.text() || "0");
+      if (rating > user.currentRating) {
+        user.score += rating - user.currentRating;
+        user.initialRating = user.currentRating;
+        user.currentRating = rating;
+        await user.save();
+      } else if (rating < user.currentRating) {
+        user.score -= user.currentRating - rating;
+        user.initialRating = user.currentRating;
+        user.currentRating = rating;
+        if (user.score < 0) user.score = 0;
+        await user.save();
+      }
+    })
+  });
+  }catch(err) {
+    console.log(err.status);
+  }
   console.log("Ratings updated")
 };
 
 const fetchData = async () => {
   const dat = []
-  const data = await axios.get("https://docs.google.com/spreadsheets/d/1E4fT40UVM8h1lZ83kNJT25QrOgrM9m7Z4COSAs6IUhE/gviz/tq?tqx=out:csv", {responseType: 'stream'})
+  const data = await axios.get("https://docs.google.com/spreadsheets/d/1E4fT40UVM8h1lZ83kNJT25QrOgrM9m7Z4COSAs6IUhE/gviz/tq?tqx=out:csv", { responseType: 'stream' })
   data.data.pipe(
-      parse({
-        delimiter: ",",
-        columns: true,
-        ltrim: true,
-      })
+    parse({
+      delimiter: ",",
+      columns: true,
+      ltrim: true,
+    })
   ).on("data", function (row) {
-      dat.push(row);
+    dat.push(row);
   }).on("error", function (error) {
-        console.log(error.message);
+    console.log(error.message);
   }).on("end", function () {
-        console.log("parsed csv data:");
-        // console.log(dat);
-        populate(dat)
-      });
+    console.log("parsed csv data:");
+    // console.log(dat);
+    populate(dat)
+  });
 
 }
 
