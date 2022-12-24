d6d46e19931b9cd87ee5533d273482aefc8452d9
Shaswat Gupta <63920595+masterchief164@users.noreply.github.com>
2022-12-24 20:07:01 +0530

    fix: added catch to the revalidate request

--- controllers/micpController.js
+++ controllers/micpController.js
@@ -15,11 +15,11 @@ exports.getRankList = async (req, res) => {
 exports.refreshRankList = async (req, res) => {
   await fetchData();
   await updateRatingsAndScores();
-  const response = await axios.get(`https://micp.netlify.app/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}`);
-  if (response?.revalidated) 
-    console.log('Cache updated');
-  else 
-    console.log('Error occured while updating cache');
+  const response = await axios.get(`https://micp.netlify.app/api/revalidate?secret=${process.env.REVALIDATE_TOKEN}`).catch((err) => {
+    console.log(err.message);
+  });
+  if (response?.revalidated) console.log('Cache updated');
+  else console.log('Error occured while updating cache');
   res.status(200).json({
     status: true,
     message: 'Refreshed',
