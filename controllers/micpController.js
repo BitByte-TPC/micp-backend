c528a320d33ef47a54dcb383237555051076c6f8
Vaishnav Deore <86405648+Waishnav@users.noreply.github.com>
2023-01-15 18:05:27 +0000

    feat: CodeForces Normalization and Leadeboard update based on score (#10)
    Co-authored-by: akshatnema <20bcs022@iiitdmj.ac.in>

--- controllers/micpController.js
+++ controllers/micpController.js
@@ -3,7 +3,7 @@ const Micp = require('../models/micp');
 const { updateRatingsAndScores, fetchData } = require('../scrap');
 
 exports.getRankList = async (req, res) => {
-  const users = await Micp.find({}, '_id username score currentRating name').sort({ score: -1, currentRating: -1 });
+  const users = await Micp.find({}, '_id email score name codeChefId codeForcesId ccCurrentRating cfCurrentRating').sort({ score: -1 });
 
   // console.log(users);
   res.status(200).json({
--- models/micp.js
+++ models/micp.js
@@ -2,22 +2,44 @@ const mongoose = require('mongoose');
 
 const micpSchema = new mongoose.Schema(
   {
-    username: {
+    email: {
       type: String,
       required: true,
       unique: true,
       trim: true,
       lowercase: true,
     },
+    codeChefId: {
+      type: String,
+      required: false,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    codeForcesId: {
+      type: String,
+      required: false,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    ccCurrentRating: {
+      type: Number,
+      default: 0,
+    },
+    ccInitialRating: {
+      type: Number,
+      default: 0,
+    },
+    cfCurrentRating: {
+      type: Number,
+      default: 0,
+    },
+    cfInitialRating: {
+      type: Number,
+      default: 0,
+    },
     score: {
-      type: Number,
-      default: 0,
-    },
-    currentRating: {
-      type: Number,
-      default: 0,
-    },
-    initialRating: {
       type: Number,
       default: 0,
     },
